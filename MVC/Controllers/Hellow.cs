1475c89ea89592af5a060fdfe7a864b01322de62
Alvianto Putra Sahefi <sahefiputra@gmail.com>
2024-05-28 23:02:25 +0700

    Add project files.

---
+++ MVC/Controllers/Hellow.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVC.Controllers
+{
+    public class Hellow : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
