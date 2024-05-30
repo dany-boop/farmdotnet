cef43bf3af92fede7d37cde02edd5c6d05946920
Dany Asmoro <dny77853@gmail.com>
2024-05-30 16:42:36 +0700

    Minor Update For Database Con

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer());
 
 var app = builder.Build();
 
